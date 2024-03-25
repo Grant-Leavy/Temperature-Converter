af01613f4cbfe5b6d93681a19ab8d6ab22ccf3a1
Chris Schuster <chris@cschuster.info>
2024-03-24 22:13:08 -0500

    first code update

--- tempconv.cpp
+++ tempconv.cpp
@@ -1,4 +1,4 @@
-// Initial Code
+// Updated code - not complete
 #include <iostream>
 
 using namespace std;
@@ -9,13 +9,26 @@ int main()
     double farenheit;
     double celsius;
     double kelvin;
-    double x, y;
+    char x, y;
+    string z;
+    
 
-    cout<<"Welcome to your favorite temperature conversion calculator!";
-    cout<<"Which temperature unit would you like to convert from? Type f for farenheit, c for celsius, or k for kelvin";
+    cout<<"Welcome to your favorite temperature conversion calculator!\n";
+    cout<<"Which temperature unit would you like to convert from? Type f for farenheit, c for celsius, or k for kelvin: ";
     cin>> x;
-    cout<<"You would like to convert "x"to which other unit?";
+    cout<<"What is the numerical value for " << x <<": ";
+    cin>>farenheit;
+    cout<< "You would like to convert ";
+    cout<< x;
+    cout<< " to which other unit? Type f for farenheit, c for celsius, or k for kelvin: ";
     cin>> y;
     
+ /*   if (x="f") {
+        z="farenheit";
+        
+  } else if (x="c") {
+      z="celsius";
+  }
+  */ 
     return 0;
 }
