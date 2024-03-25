3cee8dc13ed721319e2b2dbbc328ba00d090476e
Chris Schuster <chris@cschuster.info>
2024-03-24 22:44:01 -0500

    catching up history thru update7

--- tempconv.cpp
+++ tempconv.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 
 using namespace std;
@@ -13,35 +14,35 @@ int main()
     cout<<"Which temperature unit would you like to convert from? Type f for farenheit, c for celsius, or k for kelvin: ";
     cin>> x;
 
-     if (x='f') {
+     if (x=='f') {
         z="farenheit";
 
-  } else if (x='c') {
+  } else if (x=='c') {
       z="celsius";
-  } else if (x='k') {
+  } else if (x=='k') {
       z="kelvin";
   }
 
     cout<<"What is the numerical value for " << z <<": ";
     cin>>input;
 
-     if (x='f') {
+     if (x=='f') {
         farenheit = input;
         celsius = (5/9)*(input-32);
-        kelvin = 5/9*input+459.67;
+        kelvin = (5/9)*(input-32)+273.15;
         cout<<"celsius = " <<celsius;
         cout<<"\nkelvin = " << kelvin;
 
-  } else if (x='c') {
+  } else if (x=='c') {
         celsius = input;
-        farenheit = (5/9)*(input-32);
-        kelvin = 5/9*input+459.67;
+        farenheit = (9/5)*input+32;
+        kelvin = input+273.15;
         cout<<"farenheit = " <<farenheit;
         cout<<"\nkelvin = " << kelvin;
-  } else if (x='k') {
+  } else if (x=='k') {
         kelvin = input;
-        farenheit = (5/9)*(input-32);
-        celsius = 5/9*input+459.67;
+        farenheit = (9/5)*(input-273.15)+32;
+        celsius = input-273.15;
         cout<<"farenheit = " <<farenheit;
         cout<<"\ncelsius = " << celsius;
   }
