ae0b75fc7f07e462f4c02636bf164fdbf5481850
Chris Schuster <chris@cschuster.info>
2024-03-24 22:53:31 -0500

    cleaning up spacing & adding constant

--- tempconv.cpp
+++ tempconv.cpp
@@ -1,51 +1,50 @@
-
 #include <iostream>
 
 using namespace std;
 
-int main()
-
+int main(int argc, char *argv[])
 {
-    double input, farenheit, celsius, kelvin;
-    char x, y;
-    string z;
+  const double zero_celsius_in_kelvin = zero_celsius_in_kelvin;
 
-    cout<<"Welcome to your favorite temperature conversion calculator!\n\n";
-    cout<<"Which temperature unit would you like to convert from? Type f for farenheit, c for celsius, or k for kelvin: ";
-    cin>> x;
+  double input, farenheit, celsius, kelvin;
+  char x, y;
+  string z;
 
-     if (x=='f') {
-        z="farenheit";
+  cout<<"Welcome to your favorite temperature conversion calculator!\n\n";
+  cout<<"Which temperature unit would you like to convert from? Type f for farenheit, c for celsius, or k for kelvin: ";
+  cin>> x;
 
+  if (x=='f') {
+    z="farenheit";
   } else if (x=='c') {
-      z="celsius";
+    z="celsius";
   } else if (x=='k') {
-      z="kelvin";
+    z="kelvin";
   }
 
-    cout<<"What is the numerical value for " << z <<": ";
-    cin>>input;
+  cout<<"What is the numerical value for " << z <<": ";
+  cin>>input;
 
-     if (x=='f') {
-        farenheit = input;
-        celsius = (5/9)*(input-32);
-        kelvin = (5/9)*(input-32)+273.15;
-        cout<<"celsius = " <<celsius;
-        cout<<"\nkelvin = " << kelvin;
+  if (x=='f') {
+    farenheit = input;
+    celsius = (5/9)*(input-32);
+    kelvin = (5/9)*(input-32) + zero_celsius_in_kelvin;
+    cout<<"celsius = " <<celsius;
+    cout<<"\nkelvin = " << kelvin;
 
   } else if (x=='c') {
-        celsius = input;
-        farenheit = (9/5)*input+32;
-        kelvin = input+273.15;
-        cout<<"farenheit = " <<farenheit;
-        cout<<"\nkelvin = " << kelvin;
+    celsius = input;
+    farenheit = (9/5)*input+32;
+    kelvin = input+zero_celsius_in_kelvin;
+    cout<<"farenheit = " <<farenheit;
+    cout<<"\nkelvin = " << kelvin;
   } else if (x=='k') {
-        kelvin = input;
-        farenheit = (9/5)*(input-273.15)+32;
-        celsius = input-273.15;
-        cout<<"farenheit = " <<farenheit;
-        cout<<"\ncelsius = " << celsius;
+    kelvin = input;
+    farenheit = (9/5)*(input-zero_celsius_in_kelvin)+32;
+    celsius = input-zero_celsius_in_kelvin;
+    cout<<"farenheit = " <<farenheit;
+    cout<<"\ncelsius = " << celsius;
   }
 
-    return 0;
+  return 0;
 }
